b8f2fea620face07e8eba01a68808d2112d329a7
rohansadhukhan65 <sadhukhanrohan15@gmail.com>
2022-03-19 02:48:29 +0530

    r

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Head from "next/head";
+import { useEffect } from "react";
 import '../styles/globals.css'
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+      /* we also run "yarn add @popperjs/core" command to resolve popper.js issue */
+      import("bootstrap/dist/js/bootstrap");
+    }, []);
+  return <>
+
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Component {...pageProps} />
+    
+  </> 
 }
 
 export default MyApp
